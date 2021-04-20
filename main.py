485632bc961853ce3455409f6d98768ee3eed304
Kevin McAleer <kevinmcaleer@gmail.com>
2021-04-20 15:59:06 +0100

    Initial Walking test

--- main.py
+++ main.py
@@ -1,21 +1,54 @@
-from microbit import *
+def test_1():
+    global FOOT_LOW, FOOT_HIGH
+    foot = LEFT_BACK_FOOT
+    FOOT_LOW = 20
+    FOOT_HIGH = 150
+    Servo.servo(foot, FOOT_HIGH)
+    basic.pause(1000)
+    Servo.servo(foot, FOOT_LOW)
+def stand_up():
+    Servo.servo(LEFT_BACK_FOOT, 0)
+    Servo.servo(RIGHT_BACK_FOOT, 0)
+    Servo.servo(LEFT_FRONT_FOOT, 0)
+    Servo.servo(RIGHT_FRONT_FOOT, 0)
+def default_position():
+    # Set the robot to the default position
+    Servo.servo(LEFT_FRONT_FOOT, 90)
+    Servo.servo(LEFT_FRONT_LEG, 90)
+    Servo.servo(LEFT_BACK_FOOT, 90)
+    Servo.servo(LEFT_BACK_LEG, 90)
+    Servo.servo(RIGHT_FRONT_FOOT, 90)
+    Servo.servo(RIGHT_FRONT_LEG, 90)
+    Servo.servo(RIGHT_BACK_FOOT, 90)
+    Servo.servo(RIGHT_BACK_LEG, 90)
+    basic.show_icon(IconNames.SMALL_DIAMOND)
+LEFT_FRONT_FOOT = 0
+FOOT_HIGH = 0
+FOOT_LOW = 0
+RIGHT_BACK_LEG = 0
+RIGHT_BACK_FOOT = 0
+RIGHT_FRONT_LEG = 0
+RIGHT_FRONT_FOOT = 0
+LEFT_BACK_LEG = 0
+LEFT_FRONT_LEG = 0
+LEFT_BACK_FOOT = 0
+LEFT_BACK_FOOT = 0
+LEFT_FRONT_LEG = 1
+LEFT_BACK_FOOT = 3
+LEFT_BACK_LEG = 2
+RIGHT_FRONT_FOOT = 4
+RIGHT_FRONT_LEG = 5
+RIGHT_BACK_FOOT = 6
+RIGHT_BACK_LEG = 7
+soundExpression.hello.play()
 
-start = 0x07
-end = 0x70
-
-while True:    
-    basic.show_arrow(ArrowNames.NORTH)
+def on_forever():
     if input.button_is_pressed(Button.A):
-        basic.show_icon(IconNames.HAPPY)
-        print("scanning I2C bus...")
-        for i in range(start, end + 1):
-            try:
-                microbit.i2c.read(i,1)
-            except OSError:
-                pass
-            else:
-                print("Found: [%s]" %hex(i))
-        print("Scanning complete")
-
-
-
+        test_1()
+        basic.pause(1000)
+    if input.logo_is_pressed():
+        default_position()
+    if input.button_is_pressed(Button.B):
+        stand_up()
+    default_position()
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,10 +1,59 @@
-let start = 0x07
-let end = 0x70
-while (true) {
-    basic.showArrow(ArrowNames.North)
-    if (input.buttonIsPressed(Button.A)) {
-        basic.showIcon(IconNames.Happy)
-        console.log("scanning I2C bus...")
-    }
-    
+function test_1 () {
+    foot = LEFT_FRONT_FOOT
+    FOOT_LOW = 20
+    FOOT_HIGH = 150
+    Servo.Servo(foot, FOOT_HIGH)
+    basic.pause(1000)
+    Servo.Servo(foot, FOOT_LOW)
 }
+function stand_up () {
+    Servo.Servo(LEFT_BACK_FOOT, 0)
+    Servo.Servo(RIGHT_BACK_FOOT, 0)
+    Servo.Servo(LEFT_FRONT_FOOT, 0)
+    Servo.Servo(RIGHT_FRONT_FOOT, 0)
+}
+function default_position () {
+    // Set the robot to the default position
+    Servo.Servo(LEFT_FRONT_FOOT, 90)
+    Servo.Servo(LEFT_FRONT_LEG, 90)
+    Servo.Servo(LEFT_BACK_FOOT, 90)
+    Servo.Servo(LEFT_BACK_LEG, 90)
+    Servo.Servo(RIGHT_FRONT_FOOT, 90)
+    Servo.Servo(RIGHT_FRONT_LEG, 90)
+    Servo.Servo(RIGHT_BACK_FOOT, 90)
+    Servo.Servo(RIGHT_BACK_LEG, 90)
+    basic.showIcon(IconNames.SmallDiamond)
+}
+let FOOT_HIGH = 0
+let FOOT_LOW = 0
+let LEFT_FRONT_FOOT = 0
+let foot = 0
+let RIGHT_BACK_LEG = 0
+let RIGHT_BACK_FOOT = 0
+let RIGHT_FRONT_LEG = 0
+let RIGHT_FRONT_FOOT = 0
+let LEFT_BACK_LEG = 0
+let LEFT_FRONT_LEG = 0
+let LEFT_BACK_FOOT = 0
+LEFT_BACK_FOOT = 0
+LEFT_FRONT_LEG = 1
+LEFT_BACK_FOOT = 3
+LEFT_BACK_LEG = 2
+RIGHT_FRONT_FOOT = 4
+RIGHT_FRONT_LEG = 5
+RIGHT_BACK_FOOT = 6
+RIGHT_BACK_LEG = 7
+soundExpression.hello.play()
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        test_1()
+        basic.pause(1000)
+    }
+    if (input.logoIsPressed()) {
+        default_position()
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        stand_up()
+    }
+    default_position()
+})
